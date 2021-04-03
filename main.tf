1df98469e6c90ad6f36f5d5b1d064144d6b4a6a4
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 16:12:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 
+
+resource "kubernetes_namespace" "main" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+
+module "orderfile" {
+  source  = "./modules/orderfile"
+
+  app_name  = var.app_name
+  namespace = var.namespace
+}
