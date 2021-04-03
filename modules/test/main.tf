73f6fb8764f186d419dcc66ee18d7492104a0d0e
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 17:06:59 +0200

    Update main.tf

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,27 +1,44 @@
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+  }
+}
 
 
+############################################################
+# CREATE TEST POD
+############################################################
+resource "kubernetes_pod" "test" {
+  metadata {
+    name      = "test"
+    namespace = var.namespace
+  }
 
+  spec {
+    container {
+      name  = "test"
+      image = "busybox"
+      
+      command = ["sleep", "9999999999d"]
+      
+      resources {
+        requests = {
+          cpu    = "10m"
+          memory = "8Mi"
+        }
+        limits = {
+          cpu    = "50m"
+          memory = "32Mi"
+        }
+      }
 
-apiVersion: v1
-kind: Pod
-metadata:
-  labels:
-    run: test
-  name: test
-  namespace: {{ .Release.Namespace }}
-spec:
-  containers:
-  - name: test
-    image: busybox
-    command:
-      - sleep
-      - "9999999999d"
-    resources:
-      requests:
-        cpu: 10m
-        memory: 8Mi
-      limits:
-        cpu: 50m
-        memory: 32Mi
-  dnsPolicy: ClusterFirst
-  restartPolicy: Always
+    restart_policy = "Always"
+    dns_policy     = "ClusterFirst"
+  }
+}
