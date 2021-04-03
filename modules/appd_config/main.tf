81b0b71926c76963c823f3a79f45950bd4097d3b
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 20:53:02 +0200

    Update main.tf

--- modules/appd_config/main.tf
+++ modules/appd_config/main.tf
@@ -1,31 +1,41 @@
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: appd-config
-  namespace: {{ .Release.Namespace }}
-data:
-  ACCOUNT_NAME: {{ .Values.appd_account_name }}
-  APPD_ACCOUNT_NAME: {{ .Values.appd_account_name }} #Python
-  APPDYNAMICS_AGENT_ACCOUNT_NAME: {{ .Values.appd_account_name }} #NodeJS
-  
-  CONTROLLER_HOST: {{ .Values.appd_controller_hostname }}
-  APPD_CONTROLLER_HOST: {{ .Values.appd_controller_hostname }} #Python
-  APPDYNAMICS_CONTROLLER_HOST_NAME: {{ .Values.appd_controller_hostname }} #NodeJS
-  
-  CONTROLLER_PORT: "{{ .Values.appd_controller_port }}"
-  APPD_CONTROLLER_PORT: "{{ .Values.appd_controller_port }}" #Python
-  APPDYNAMICS_CONTROLLER_PORT: "{{ .Values.appd_controller_port }}" #NodeJS
+
+
+resource "kubernetes_config_map" "example" {
+  metadata {
+    name      = "appd-config"
+    namespace = var.namespace
+  }
+
+  data = {
+    APP_NAME                           = var.appname
+    APPDYNAMICS_AGENT_APPLICATION_NAME = var.appname #NodeJS
+    
+    BROWSERAPP_KEY       = var.appd_browserapp_key
+    BROWSERAPP_BEACONURL = var.appd_browserapp_beaconurl
+    
+    ACCOUNT_NAME                   = var.appd_account_name
+    APPD_ACCOUNT_NAME              = var.appd_account_name #Python
+    APPDYNAMICS_AGENT_ACCOUNT_NAME = var.appd_account_name #NodeJS
+    
+    CONTROLLER_HOST                  = var.appd_controller_hostname
+    APPD_CONTROLLER_HOST             = var.appd_controller_hostname #Python
+    APPDYNAMICS_CONTROLLER_HOST_NAME = var.appd_controller_hostname # NodeJS
+    
+    CONTROLLER_PORT             = var.appd_controller_port
+    APPD_CONTROLLER_PORT        = var.appd_controller_port #Python
+    APPDYNAMICS_CONTROLLER_PORT = var.appd_controller_port #NodeJS
+    
+    ACCESS_KEY                           = var.appd_controller_key
+    APPD_ACCOUNT_ACCESS_KEY              = var.appd_controller_key #Python
+    APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY = var.appd_controller_key #NodeJS
+  }
+}
+
+
   
   CONTROLLER_SSL: "{{ .Values.appd_controller_ssl }}"
   APPD_SSL_ENABLED: "on" #Python
   APPDYNAMICS_CONTROLLER_SSL_ENABLED: "{{ .Values.appd_controller_ssl }}" #NodeJS
-  
-  ACCESS_KEY: {{ .Values.appd_controller_key }}
-  APPD_ACCOUNT_ACCESS_KEY: {{ .Values.appd_controller_key }} #Python
-  APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY: {{ .Values.appd_controller_key }} #NodeJS
-  
-  APP_NAME: {{ .Values.appname }}
-  APPDYNAMICS_AGENT_APPLICATION_NAME: {{ .Values.appname }} #NodeJS
   
   {{- if .Values.proxy_host }}
   # PROXY HOST
@@ -39,7 +49,3 @@ data:
   APPD_HTTP_PROXY_PORT: "{{ .Values.proxy_port }}" #Python
   
   {{- end }}
-  
-  BROWSERAPP_KEY: {{ .Values.appd_browserapp_key }}
-  
-  BROWSERAPP_BEACONURL: {{ .Values.appd_browserapp_beaconurl }}
