88de46acdc9a76fbd19d10238159cf1f2cee2fbc
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 22:37:10 +0200

    Update main.tf

--- modules/appd_config/main.tf
+++ modules/appd_config/main.tf
@@ -1,6 +1,9 @@
-
-
-resource "kubernetes_config_map" "example" {
+############################################################
+# CREATE CONFIGMAP WITHOUT PROXY
+############################################################
+resource "kubernetes_config_map" "without-proxy" {
+  count = var.use_proxy ? 0 : 1
+  
   metadata {
     name      = "appd-config"
     namespace = var.namespace
@@ -28,24 +31,56 @@ resource "kubernetes_config_map" "example" {
     ACCESS_KEY                           = var.appd_controller_key
     APPD_ACCOUNT_ACCESS_KEY              = var.appd_controller_key #Python
     APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY = var.appd_controller_key #NodeJS
+    
+    CONTROLLER_SSL                     = var.appd_controller_ssl
+    APPD_SSL_ENABLED                   = "on" #Python
+    APPDYNAMICS_CONTROLLER_SSL_ENABLED = var.appd_controller_ssl #NodeJS
   }
 }
 
 
+############################################################
+# CREATE CONFIGMAP WITHOUT PROXY
+############################################################
+resource "kubernetes_config_map" "with-proxy" {
+  count = var.use_proxy ? 1 : 0
   
-  CONTROLLER_SSL: "{{ .Values.appd_controller_ssl }}"
-  APPD_SSL_ENABLED: "on" #Python
-  APPDYNAMICS_CONTROLLER_SSL_ENABLED: "{{ .Values.appd_controller_ssl }}" #NodeJS
-  
-  {{- if .Values.proxy_host }}
-  # PROXY HOST
-  PROXY_HOST: {{ .Values.proxy_host }}
-  APPD_HTTP_PROXY_HOST: {{ .Values.proxy_host }} #Python
-  {{- end }}
-  
-  {{- if .Values.proxy_port }}
-  # PROXY PORT
-  PROXY_PORT: "{{ .Values.proxy_port }}"
-  APPD_HTTP_PROXY_PORT: "{{ .Values.proxy_port }}" #Python
-  
-  {{- end }}
+  metadata {
+    name      = "appd-config"
+    namespace = var.namespace
+  }
+
+  data = {
+    APP_NAME                           = var.appname
+    APPDYNAMICS_AGENT_APPLICATION_NAME = var.appname #NodeJS
+    
+    BROWSERAPP_KEY       = var.appd_browserapp_key
+    BROWSERAPP_BEACONURL = var.appd_browserapp_beaconurl
+    
+    ACCOUNT_NAME                   = var.appd_account_name
+    APPD_ACCOUNT_NAME              = var.appd_account_name #Python
+    APPDYNAMICS_AGENT_ACCOUNT_NAME = var.appd_account_name #NodeJS
+    
+    CONTROLLER_HOST                  = var.appd_controller_hostname
+    APPD_CONTROLLER_HOST             = var.appd_controller_hostname #Python
+    APPDYNAMICS_CONTROLLER_HOST_NAME = var.appd_controller_hostname # NodeJS
+    
+    CONTROLLER_PORT             = var.appd_controller_port
+    APPD_CONTROLLER_PORT        = var.appd_controller_port #Python
+    APPDYNAMICS_CONTROLLER_PORT = var.appd_controller_port #NodeJS
+    
+    ACCESS_KEY                           = var.appd_controller_key
+    APPD_ACCOUNT_ACCESS_KEY              = var.appd_controller_key #Python
+    APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY = var.appd_controller_key #NodeJS
+    
+    CONTROLLER_SSL                     = var.appd_controller_ssl
+    APPD_SSL_ENABLED                   = "on" #Python
+    APPDYNAMICS_CONTROLLER_SSL_ENABLED = var.appd_controller_ssl #NodeJS
+    
+    PROXY_HOST           = var.proxy_host
+    APPD_HTTP_PROXY_HOST = var.proxy_host #Python
+    
+    PROXY_PORT           = var.proxy_port
+    APPD_HTTP_PROXY_PORT = var.proxy_port #Python
+  }
+}
