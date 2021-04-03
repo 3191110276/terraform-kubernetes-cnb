08a99694653d2a95860fe1518942840e4059bc00
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-03 16:10:58 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+############################################################
+# INPUT VARIABLES
+############################################################
+variable "namespace" {
+  type        = string
+  default     = "brewery"
+  description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
+}
 
+variable "app_name" {
+  type        = string
+  default     = "brewery"
+  description = "The name of the application that this submodule is part of."
+}
