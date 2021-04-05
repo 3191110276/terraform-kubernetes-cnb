33f82ec952a0c60ad3c2a765235cf3c29bd70d01
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-05 13:46:20 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 ############################################################
-# INPUT VARIABLES
+# GENERAL
 ############################################################
 variable "namespace" {
   type        = string
@@ -13,6 +13,10 @@ variable "app_name" {
   description = "The name of the application that this submodule is part of."
 }
 
+
+############################################################
+# APPDYNAMICS
+############################################################
 variable "appd_browserapp_key" {
   type        = string
   description = "Key for the AppDynamics EUM application."
@@ -65,4 +69,56 @@ variable "proxy_port" {
   type        = string
   default     = "443"
   description = "Proxy port for connections to the AppDynamics controller. Only needed if the connection uses proxy."
+}
+
+
+############################################################
+# CUSTOMIZATION
+############################################################
+variable "inventorydb_service" {
+  type        = string
+  default     = "cnb-inventorydb-service"
+  description = "Service for the InventoryDB application component."
+}
+
+variable "payment_service" {
+  type        = string
+  default     = "cnb-payment"
+  description = "Service for the Payment application component."
+}
+
+variable "extpayment_service" {
+  type        = string
+  default     = "payment.ext"
+  description = "Service for the ExtPayment application component."
+}
+
+variable "initqueue_service" {
+  type        = string
+  default     = "cnb-initqueue-rabbitmq"
+  description = "Service for the Initqueue application component."
+}
+
+variable "orderprocessing_service" {
+  type        = string
+  default     = "cnb-orderprocessing"
+  description = "Service for the Orderprocessing application component."
+}
+
+variable "production_service" {
+  type        = string
+  default     = "cnb-production"
+  description = "Service for the Production application component."
+}
+
+variable "extprod_service" {
+  type        = string
+  default     = "production.automation"
+  description = "Service for the ExtProd application component."
+}
+
+variable "fulfilment_service" {
+  type        = string
+  default     = "cnb-fulfilment"
+  description = "Service for the Fulfilment application component."
 }
