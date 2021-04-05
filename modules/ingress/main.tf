fa37c46c4a38457950d53e94a5e19e2c581e1c52
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-05 14:59:27 +0200

    Update main.tf

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -1,1 +1,43 @@
+resource "kubernetes_ingress" "ingress" {
+  metadata {
+    name = "${var.app_name}-ingress"
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/$2$3"
+    }
+  }
 
+  spec {
+    rule {
+      http {
+        path {
+          path = "/()()(.*)"
+          backend {
+            service_name = "${var.app_name}-${var.orderfile_name}"
+            service_port = 80
+          }
+        }
+        path {
+          path = "/(admin)(/|$)(.*)"
+          backend {
+            service_name = "${var.app_name}-${var.adminfile_name}"
+            service_port = 80
+          }
+        }
+        path {
+          path = "/(api)(/)(order)"
+          backend {
+            service_name = "${var.app_name}-${var.order_name}"
+            service_port = 80
+          }
+        }
+        path {
+          path = "/(rabbitmq)(/|$)(.*)"
+          backend {
+            service_name = "${var.app_name}-${var.initqueue_name}-rabbitmq"
+            service_port = 15672
+          }
+        }
+      }
+    }
+  }
+}
