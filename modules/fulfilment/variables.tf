012dadd20343a6e0d401110f0c5d2bf3a8c54f44
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-06 17:14:44 +0200

    Update variables.tf

--- modules/fulfilment/variables.tf
+++ modules/fulfilment/variables.tf
@@ -1,1 +1,74 @@
+############################################################
+# INPUT VARIABLES
+############################################################
+variable "namespace" {
+  type        = string
+  default     = "brewery"
+  description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
+}
 
+variable "app_name" {
+  type        = string
+  default     = "cnb"
+  description = "The name of the application that this submodule is part of."
+}
+
+variable "fulfilment_name" {
+  type        = string
+  default     = "fulfilment"
+  description = "The name of the application component deployed through this submodule. Changing this value will change how the application component is called in Kubernetes."
+}
+
+variable "fulfilment_appd" {
+  type        = string
+  default     = "Fulfilment"
+  description = "The name of the application component deployed through this submodule. Changing this value will change how the application component is called in AppDynamics."
+}
+
+variable "replicas" {
+  type        = number
+  default     = 2
+  description = "The number of replicas that should initially be deployed for this component."
+}
+
+variable "cpu_request" {
+  type        = string
+  default     = "100m"
+  description = "The value for requests.cpu."
+}
+
+variable "memory_request" {
+  type        = string
+  default     = "128Mi"
+  description = "The value for requests.memory."
+}
+
+variable "cpu_limit" {
+  type        = string
+  default     = "400m"
+  description = "The value for limits.cpu."
+}
+
+variable "memory_limit" {
+  type        = string
+  default     = "512Mi"
+  description = "The value for limits.memory."
+}
+
+variable "registry" {
+  type        = string
+  default     = "mimaurer"
+  description = "The registry from which the application image is pulled."
+}
+
+variable "order_tech" {
+  type        = string
+  default     = "python"
+  description = "The image technology."
+}
+
+variable "order_version" {
+  type        = string
+  default     = "master"
+  description = "The version tag referencing the image that will be pulled from the registry"
+}
