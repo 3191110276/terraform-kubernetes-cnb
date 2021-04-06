366ceb87974e1fffb6d0925d9f1a1a9da0fcfe48
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-06 22:22:07 +0200

    Update main.tf

--- modules/orderqueue/main.tf
+++ modules/orderqueue/main.tf
@@ -14,7 +14,7 @@ terraform {
 ############################################################
 # CREATE ORDERQUEUE DEPLOYMENT
 ############################################################
-resource "kubernetes_service" "example" {
+resource "kubernetes_service" "orderqueue_headless" {
   metadata {
     name = "${var.app_name}-${var.initqueue_name}-rabbitmq-headless"
     namespace = var.namespace
@@ -53,7 +53,7 @@ resource "kubernetes_service" "example" {
 }
 
 
-resource "kubernetes_service" "example" {
+resource "kubernetes_service" "orderqueue" {
   metadata {
     name = "${var.app_name}-${var.initqueue_name}-rabbitmq"
     namespace = var.namespace
@@ -89,6 +89,182 @@ resource "kubernetes_service" "example" {
     type = "ClusterIP"
   }
 }
+
+
+
+
+
+
+
+
+
+resource "kubernetes_stateful_set" "orderqueue" {
+  depends_on = [kubernetes_service.orderqueue_headless, kubernetes_service.orderqueue, kubernetes_pod_disruption_budget.orderqueue, kubernetes_secret.orderqueue, kubernetes_role_binding.orderqueue, kubernetes_config_map.orderqueue]
+  
+  metadata {
+    name      = "${var.app_name}-${var.initqueue_name}-rabbitmq"
+    namespace = var.namespace
+  }
+
+  spec {
+    pod_management_policy  = "OrderedReady"
+    replicas               = 3
+    
+    update_strategy {
+      type = "RollingUpdate"
+    }
+
+    selector {
+      match_labels = {
+        tier = var.initqueue_name
+      }
+    }
+
+    service_name = "${var.app_name}-${var.initqueue_name}-rabbitmq-headless"
+
+    template {
+      metadata {
+        labels = {
+          tier = var.initqueue_name
+        }
+        
+        annotations = {
+          "checksum/config" = "0a4e272ea944acede38be298ecdf4ce8b4a2b83f32c2015e815c4ee1d4c78162"
+          "checksum/secret" = "cdf4233d6b9f1dab011c2fd778cf07f09c0565cdf595f41c2ddb275baad49e5c"
+        }
+      }
+
+      spec {
+        service_account_name = "${var.app_name}-${var.initqueue_name}-rabbitmq"
+        
+        termination_grace_period_seconds = 120
+
+        
+        
+        
+        
+        
+        container {
+          name              = "prometheus-server"
+          image             = "prom/prometheus:v2.2.1"
+          image_pull_policy = "IfNotPresent"
+
+          args = [
+            "--config.file=/etc/config/prometheus.yml",
+            "--storage.tsdb.path=/data",
+            "--web.console.libraries=/etc/prometheus/console_libraries",
+            "--web.console.templates=/etc/prometheus/consoles",
+            "--web.enable-lifecycle",
+          ]
+
+          port {
+            container_port = 9090
+          }
+
+          resources {
+            limits = {
+              cpu    = "200m"
+              memory = "1000Mi"
+            }
+
+            requests = {
+              cpu    = "200m"
+              memory = "1000Mi"
+            }
+          }
+
+          volume_mount {
+            name       = "config-volume"
+            mount_path = "/etc/config"
+          }
+
+          volume_mount {
+            name       = "prometheus-data"
+            mount_path = "/data"
+            sub_path   = ""
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/-/ready"
+              port = 9090
+            }
+
+            initial_delay_seconds = 30
+            timeout_seconds       = 30
+          }
+
+          liveness_probe {
+            http_get {
+              path   = "/-/healthy"
+              port   = 9090
+              scheme = "HTTPS"
+            }
+
+            initial_delay_seconds = 30
+            timeout_seconds       = 30
+          }
+        }
+
+        volume {
+          name = "config-volume"
+
+          config_map {
+            name = "prometheus-config"
+          }
+        }
+      }
+    }
+
+    
+    
+    
+    
+    
+    volume_claim_template {
+      metadata {
+        name = "data"
+        
+        labels = {
+          "app.kubernetes.io/name" = "rabbitmq"
+          "app.kubernetes.io/instance" = "${var.app_name}-${var.initqueue_name}"
+        }
+      }
+
+      spec {
+        access_modes = ["ReadWriteOnce"]
+
+        resources {
+          requests = {
+            storage = "8Gi"
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 resource "kubernetes_pod_disruption_budget" "orderqueue" {
@@ -200,176 +376,3 @@ resource "kubernetes_config_map" "orderqueue" {
 
 
 
----
-apiVersion: apps/v1
-kind: StatefulSet
-metadata:
-  name: {{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq
-  namespace: {{ .Release.Namespace }}
-spec:
-  serviceName: {{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq-headless
-  podManagementPolicy: OrderedReady
-  replicas: 3
-  updateStrategy:
-    type: RollingUpdate
-  selector:
-    matchLabels:
-      tier: {{ .Values.initqueue_name }}
-  template:
-    metadata:
-      labels:
-        tier: {{ .Values.initqueue_name }}
-      annotations:
-        checksum/config: 0a4e272ea944acede38be298ecdf4ce8b4a2b83f32c2015e815c4ee1d4c78162
-        checksum/secret: cdf4233d6b9f1dab011c2fd778cf07f09c0565cdf595f41c2ddb275baad49e5c
-    spec:
-      
-      serviceAccountName: {{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq
-      affinity:
-        podAffinity:
-          
-        podAntiAffinity:
-          preferredDuringSchedulingIgnoredDuringExecution:
-            - podAffinityTerm:
-                labelSelector:
-                  matchLabels:
-                    tier: {{ .Values.initqueue_name }}
-                namespaces:
-                  - {{ .Release.Namespace }}
-                topologyKey: kubernetes.io/hostname
-              weight: 1
-        nodeAffinity:
-          
-      securityContext:
-        fsGroup: 1001
-        runAsUser: 1001
-      terminationGracePeriodSeconds: 120
-      containers:
-        - name: rabbitmq
-          image: docker.io/bitnami/rabbitmq:3.8.9-debian-10-r64
-          imagePullPolicy: "IfNotPresent"
-          env:
-            - name: BITNAMI_DEBUG
-              value: "false"
-            - name: MY_POD_IP
-              valueFrom:
-                fieldRef:
-                  fieldPath: status.podIP
-            - name: MY_POD_NAME
-              valueFrom:
-                fieldRef:
-                  fieldPath: metadata.name
-            - name: MY_POD_NAMESPACE
-              valueFrom:
-                fieldRef:
-                  fieldPath: metadata.namespace
-            - name: K8S_SERVICE_NAME
-              value: "{{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq-headless"
-            - name: K8S_ADDRESS_TYPE
-              value: hostname
-            - name: RABBITMQ_FORCE_BOOT
-              value: "no"
-            - name: RABBITMQ_NODE_NAME
-              value: "rabbit@$(MY_POD_NAME).$(K8S_SERVICE_NAME).$(MY_POD_NAMESPACE).svc.cluster.local"
-            - name: K8S_HOSTNAME_SUFFIX
-              value: ".$(K8S_SERVICE_NAME).$(MY_POD_NAMESPACE).svc.cluster.local"
-            - name: RABBITMQ_MNESIA_DIR
-              value: "/bitnami/rabbitmq/mnesia/$(RABBITMQ_NODE_NAME)"
-            - name: RABBITMQ_LDAP_ENABLE
-              value: "no"
-            - name: RABBITMQ_LOGS
-              value: "-"
-            - name: RABBITMQ_ULIMIT_NOFILES
-              value: "65536"
-            - name: RABBITMQ_USE_LONGNAME
-              value: "true"
-            - name: RABBITMQ_ERL_COOKIE
-              valueFrom:
-                secretKeyRef:
-                  name: {{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq
-                  key: rabbitmq-erlang-cookie
-            - name: RABBITMQ_USERNAME
-#              value: "user"
-              value: "guest"
-            - name: RABBITMQ_PASSWORD
-              value: "guest"
-#              valueFrom:
-#                secretKeyRef:
-#                  name: {{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq
-#                  key: rabbitmq-password
-            - name: RABBITMQ_PLUGINS
-              value: "rabbitmq_management, rabbitmq_peer_discovery_k8s, rabbitmq_auth_backend_ldap"
-          ports:
-            - name: amqp
-              containerPort: 5672
-            - name: dist
-              containerPort: 25672
-            - name: stats
-              containerPort: 15672
-            - name: epmd
-              containerPort: 4369
-          livenessProbe:
-            exec:
-              command:
-                - /bin/bash
-                - -ec
-                - rabbitmq-diagnostics -q ping
-            initialDelaySeconds: 120
-            periodSeconds: 30
-            timeoutSeconds: 20
-            successThreshold: 1
-            failureThreshold: 3
-          readinessProbe:
-            exec:
-              command:
-                - /bin/bash
-                - -ec
-                - rabbitmq-diagnostics -q check_running && rabbitmq-diagnostics -q check_local_alarms
-            initialDelaySeconds: 10
-            periodSeconds: 30
-            timeoutSeconds: 20
-            successThreshold: 1
-            failureThreshold: 3
-          resources:
-            limits:
-              cpu: 1000m
-              memory: 2Gi
-            requests:
-              cpu: 1000m
-              memory: 2Gi
-          lifecycle:
-            preStop:
-              exec:
-                command:
-                  - /bin/bash
-                  - -ec
-                  - |
-                    if [[ -f /opt/bitnami/scripts/rabbitmq/nodeshutdown.sh ]]; then
-                        /opt/bitnami/scripts/rabbitmq/nodeshutdown.sh -t "120" -d  "false"
-                    else
-                        rabbitmqctl stop_app
-                    fi
-          volumeMounts:
-            - name: configuration
-              mountPath: /bitnami/rabbitmq/conf
-            - name: data
-              mountPath: /bitnami/rabbitmq/mnesia
-      volumes:
-        - name: configuration
-          configMap:
-            name: {{ .Values.appname }}-{{ .Values.initqueue_name }}-rabbitmq-config
-            items:
-              - key: rabbitmq.conf
-                path: rabbitmq.conf
-  volumeClaimTemplates:
-    - metadata:
-        name: data
-        labels:
-          app.kubernetes.io/name: rabbitmq
-          app.kubernetes.io/instance: {{ .Values.appname }}-{{ .Values.initqueue_name }}
-      spec:
-        accessModes:
-          - "ReadWriteOnce"
-        resources:
-          requests:
-            storage: "8Gi"
