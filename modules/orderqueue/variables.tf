6733e5d038e0bcdeb48589edd303305dfffa5c7a
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-07 09:47:49 +0200

    Update variables.tf

--- modules/orderqueue/variables.tf
+++ modules/orderqueue/variables.tf
@@ -6,3 +6,15 @@ variable "namespace" {
   default     = "brewery"
   description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
 }
+
+variable "app_name" {
+  type        = string
+  default     = "cnb"
+  description = "The name of the application that this submodule is part of."
+}
+
+variable "initqueue_name" {
+  type        = string
+  default     = "orderqueue"
+  description = "The name of the application component deployed through this submodule. Changing this value will change how the application component is called in Kubernetes."
+}
