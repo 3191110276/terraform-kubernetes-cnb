a5e2b91acdaaeb066f4c9ccde0a18933bb4fccbd
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-10 16:13:59 +0200

    Update variables.tf

--- modules/inventorydb/variables.tf
+++ modules/inventorydb/variables.tf
@@ -1,1 +1,20 @@
+############################################################
+# INPUT VARIABLES
+############################################################
+variable "namespace" {
+  type        = string
+  default     = "brewery"
+  description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
+}
 
+variable "app_name" {
+  type        = string
+  default     = "cnb"
+  description = "The name of the application that this submodule is part of."
+}
+
+variable "inventorydb_name" {
+  type        = string
+  default     = "inventorydb"
+  description = "The name of the application component deployed through this submodule. Changing this value will change how the application component is called in Kubernetes."
+}
