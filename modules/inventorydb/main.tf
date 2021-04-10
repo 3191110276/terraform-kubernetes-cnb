ea9f515ca87dad72dbdea4963798e2147f72786b
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-10 16:15:05 +0200

    Update main.tf

--- modules/inventorydb/main.tf
+++ modules/inventorydb/main.tf
@@ -1,1 +1,21 @@
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubernetes-alpha = {
+      source  = "hashicorp/kubernetes-alpha"
+      version = ">= 0.3.2"
+    }
+  }
+}
+
+
+############################################################
+# CREATE INVENTORYDB
+############################################################
 
