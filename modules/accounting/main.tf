514195f02639a893533497e858b0b4a31ac6212f
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-12 21:28:15 +0200

    Update main.tf

--- modules/accounting/main.tf
+++ modules/accounting/main.tf
@@ -16,59 +16,9 @@ terraform {
 ############################################################
 module "clusterload" {
   source  = "3191110276/clusterload/kubernetes"
-  version = "0.2.0"
+  version = "0.2.1"
 
   namespace = var.namespace
 
-  clusterload_configurations = [{
-    pod_name = "invoicing"
-    pod_replicas = 2
-    containers = [
-      {
-      name = "webserver"
-      run_type = "cpu"
-      run_scaler = "CPU_PERCENT"
-      run_value = "10"
-      cpu_request = "240m"
-      cpu_limit = "280m"
-      mem_request = "100Mi"
-      mem_limit = "128Mi"
-      },
-      {
-      name = "processing"
-      run_type= "memory"
-      run_scaler = "MEMORY_NUM"
-      run_value = "960"
-      cpu_request = "50m"
-      cpu_limit = "800m"
-      mem_request = "1Gi"
-      mem_limit = "1Gi"
-      },
-    ]
-  },{
-    pod_name = "payroll"
-    pod_replicas = 2
-    containers = [
-      {
-      name = "webserver"
-      run_type = "cpu"
-      run_scaler = "CPU_PERCENT"
-      run_value = "95"
-      cpu_request = "50m"
-      cpu_limit = "100m"
-      mem_request = "100Mi"
-      mem_limit = "128Mi"
-      },
-      {
-      name = "notifications"
-      run_type= "memory"
-      run_scaler = "MEMORY_NUM"
-      run_value = "40"
-      cpu_request = "200m"
-      cpu_limit = "800m"
-      mem_request = "500Mi"
-      mem_limit = "500Mi"
-      },
-    ]
-  }]
+  clusterload_configurations = var.clusterload_configurations
 }
