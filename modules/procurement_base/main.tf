4947270ef28e9c872901dfce57a41f4e5a45c489
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 15:37:28 +0200

    Update main.tf

--- modules/procurement_base/main.tf
+++ modules/procurement_base/main.tf
@@ -20,3 +20,21 @@ resource "kubernetes_service_account" "appdynamics-cluster-agent" {
     namespace = var.namespace
   }
 }
+
+resource "kubernetes_resource_quota" "procurement" {
+  metadata {
+    name      = "terraform-example"
+    namespace = var.namespace
+  }
+  spec {
+    hard = {
+      pods = 100
+      "requests.cpu" = "6"
+      "limits.cpu" = "6"
+      "requests.memory" = "30G"
+      "limits.memory" = "30G"
+      "persistentvolumeclaims" = 5
+      "requests.storage" = "100G"
+    }
+  }
+}
