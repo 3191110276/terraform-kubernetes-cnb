d0dfef86ad007610fa7744f837be70dc5e2d71d2
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 13:28:26 +0200

    Update main.tf

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -14,28 +14,28 @@ resource "kubernetes_ingress" "ingress" {
         path {
           path = "/()()(.*)"
           backend {
-            service_name = "${var.app_name}-${var.orderfile_name}"
+            service_name = "${var.orderfile_name}"
             service_port = 80
           }
         }
         path {
           path = "/(admin)(/|$)(.*)"
           backend {
-            service_name = "${var.app_name}-${var.adminfile_name}"
+            service_name = "${var.adminfile_name}"
             service_port = 80
           }
         }
         path {
           path = "/(api)(/)(order)"
           backend {
-            service_name = "${var.app_name}-${var.order_name}"
+            service_name = "${var.order_name}"
             service_port = 80
           }
         }
         path {
           path = "/(rabbitmq)(/|$)(.*)"
           backend {
-            service_name = "${var.app_name}-${var.initqueue_name}-rabbitmq"
+            service_name = "${var.initqueue_name}-rabbitmq"
             service_port = 15672
           }
         }
