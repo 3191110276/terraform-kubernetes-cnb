6e6cc0113f9f23c3151f76777f1583d4534e2e95
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 14:59:58 +0200

    Update 3_accounting.tf

--- 3_accounting.tf
+++ 3_accounting.tf
@@ -1,1 +1,12 @@
+############################################################
+# ACCOUNTING APPLICATION
+############################################################
+module "accounting" {
+  depends_on = [module.test]
+  
+  source  = "./modules/accounting"
 
+  namespace = var.accounting_namespace
+
+  clusterload_configurations = var.accounting_clusterload_configurations
+}
