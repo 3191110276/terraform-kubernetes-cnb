ece8e8b50ec9057286c3bc8884245149d611e417
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 23:19:58 +0200

    Update main.tf

--- modules/procurement_edge/main.tf
+++ modules/procurement_edge/main.tf
@@ -1,58 +1,107 @@
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+  }
+}
 
 
+############################################################
+# CREATE PROCUREMENT EDGE DEPLOYMENT
+############################################################
+resource "kubernetes_service" "edge" {
+  metadata {
+    name      = "world-portal"
+    namespace = var.namespace
+  }
+  spec {
+    type = "ClusterIP"
+    
+    selector = {
+      name  = var.procportal_name
+    }
+    
+    port {
+      name        = "8080"
+      port        = 8080
+      target_port = 8080
+    }
+  }
+}
 
 
-
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: {{ .Values.component1 }}
-spec:
-  selector:
-    matchLabels:
-      name: client-api
-  replicas: {{ .Values.component1_replicas }}
-  template:
-    metadata:
-      labels:
-        name: client-api
-        tier-name: client-api-tier
-        application-name: OS-App
-    spec:
-      securityContext:
-        runAsUser: 1000
-        runAsGroup: 2000
-      serviceAccountName: appdynamics-cluster-agent
-      containers:
-        - name: {{ .Values.component1 }}
-          image: sashaz/java-services:v5
-          imagePullPolicy: IfNotPresent
-          resources:
-            limits:
-              cpu: 200m
-              memory: 900M
-            requests:
-              cpu: 100m
-              memory: 600M
-          ports:
-          - containerPort: 8080
-      restartPolicy: Always
+resource "kubernetes_service" "frontend" {
+  metadata {
+    name      = "client-api"
+    namespace = var.namespace
+  }
+  spec {
+    type = "ClusterIP"
+    
+    selector = {
+      name  = var.procedge_name
+    }
+    
+    port {
+      name        = "8080"
+      port        = 8080
+      target_port = 8080
+    }
+  }
+}
 
 
+resource "kubernetes_deployment" "order" {
+  metadata {
+    name      = var.procedge_name
+    namespace = var.namespace
+  }
 
+  spec {
+    replicas = var.replicas
 
+    selector {
+      match_labels = {
+        name  = var.procedge_name
+      }
+    }
 
+    template {
+      metadata {
+        labels = {
+          name  = var.procedge_name
+        }
+      }
 
-apiVersion: v1
-kind: Service
-metadata:
-  name: client-api
-spec:
-  selector:
-    name: client-api
-  ports:
-  - name: "8080"
-    port: 8080
-    targetPort: 8080
-status:
-  loadBalancer: {}
+      spec {
+        service_account_name = "appdynamics-cluster-agent"
+        
+        container {
+          name  = var.procedge_name
+          
+          image = "sashaz/java-services:v5"
+          
+          port {
+            container_port = 8080
+          }
+
+          resources {
+            requests = {
+              cpu    = var.cpu_request
+              memory = var.memory_request
+            }
+            limits = {
+              cpu    = var.cpu_limit
+              memory = var.memory_limit
+            }
+          }
+        }
+      }
+    }
+  }
+}
