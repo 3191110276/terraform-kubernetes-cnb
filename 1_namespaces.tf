02e45867559074a317232269b0a059d1550c7e39
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 14:58:03 +0200

    Update 1_namespaces.tf

--- 1_namespaces.tf
+++ 1_namespaces.tf
@@ -1,1 +1,43 @@
+############################################################
+# KUBERNETES NAMESPACES
+############################################################
+resource "kubernetes_namespace" "order" {
+  metadata {
+    name = var.order_namespace
+  }
+}
 
+
+resource "kubernetes_namespace" "trafficgen" {
+  metadata {
+    name = var.trafficgen_namespace
+  }
+}
+
+
+resource "kubernetes_namespace" "extprod" {
+  metadata {
+    name = var.extprod_namespace
+  }
+}
+
+
+resource "kubernetes_namespace" "extpayment" {
+  metadata {
+    name = var.extpayment_namespace
+  }
+}
+
+
+resource "kubernetes_namespace" "accounting" {
+  metadata {
+    name = var.accounting_namespace
+  }
+}
+
+
+resource "kubernetes_namespace" "procurement" {
+  metadata {
+    name = var.procurement_namespace
+  }
+}
