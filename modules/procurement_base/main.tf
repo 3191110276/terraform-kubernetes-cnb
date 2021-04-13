096bbb353184e9c2802311034a66dcba7339606a
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 15:05:56 +0200

    Update main.tf

--- modules/procurement_base/main.tf
+++ modules/procurement_base/main.tf
@@ -1,1 +1,22 @@
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+  }
+}
 
+
+############################################################
+# CREATE PROCUREMENT BASIC OBJECTS
+############################################################
+resource "kubernetes_service_account" "appdynamics-cluster-agent" {
+  metadata {
+    name      = "appdynamics-cluster-agent"
+    namespace = var.namespace
+  }
+}
