73cfb3723e54a4426739cdc078494db595bcad54
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 14:00:11 +0200

    Update main.tf

--- modules/orderfile/main.tf
+++ modules/orderfile/main.tf
@@ -16,7 +16,7 @@ terraform {
 ############################################################
 resource "kubernetes_service" "orderfile" {
   metadata {
-    name      = "${var.orderfile_name}"
+    name      = var.orderfile_name
     namespace = var.namespace
     
     labels = {
