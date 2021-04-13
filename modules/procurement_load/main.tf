c914150359c5b2c6355238f4e73748cd6e13c1b4
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-14 00:10:14 +0200

    Update main.tf

--- modules/procurement_load/main.tf
+++ modules/procurement_load/main.tf
@@ -1,1 +1,66 @@
 
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+  }
+}
+
+
+############################################################
+# CREATE PROCUREMENT LOAD DEPLOYMENT
+############################################################
+resource "kubernetes_deployment" "load" {
+  metadata {
+    name      = var.procload_name
+    namespace = var.namespace
+  }
+
+  spec {
+    replicas = var.replicas
+
+    selector {
+      match_labels = {
+        name  = var.procload_name
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          name  = var.procload_name
+        }
+      }
+
+      spec {
+        service_account_name = "appdynamics-cluster-agent"
+        
+        container {
+          name  = var.procload_name
+          
+          image = "sashaz/java-services:v5"
+          
+          port {
+            container_port = 8080
+          }
+
+          resources {
+            requests = {
+              cpu    = var.cpu_request
+              memory = var.memory_request
+            }
+            limits = {
+              cpu    = var.cpu_limit
+              memory = var.memory_limit
+            }
+          }
+        }
+      }
+    }
+  }
+}
