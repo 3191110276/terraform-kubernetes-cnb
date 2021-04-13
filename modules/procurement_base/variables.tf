9febec3a1b8c701a32f79d51a13cfa8c4c61c76f
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 21:39:24 +0200

    Update variables.tf

--- modules/procurement_base/variables.tf
+++ modules/procurement_base/variables.tf
@@ -6,3 +6,9 @@ variable "namespace" {
   default     = "procurement"
   description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
 }
+
+variable "pod_quota" {
+  type        = number
+  default     = 100
+  description = "Quota for Pods"
+}
