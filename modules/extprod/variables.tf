5d96e22c829bf8237f96f3ada2de88223cb3a54c
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 13:29:29 +0200

    Update variables.tf

--- modules/extprod/variables.tf
+++ modules/extprod/variables.tf
@@ -93,6 +93,6 @@ variable "job_max_delay" {
 
 variable "production_svc" {
   type        = string
-  default     = "cnb-production.order"
+  default     = "production.order"
   description = "The service name for the Order production application component."
 }
