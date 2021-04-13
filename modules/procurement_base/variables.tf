e7512f2eb8d2777c4949a542208de630a678cab0
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 15:06:30 +0200

    Update variables.tf

--- modules/procurement_base/variables.tf
+++ modules/procurement_base/variables.tf
@@ -1,1 +1,8 @@
-
+############################################################
+# INPUT VARIABLES
+############################################################
+variable "namespace" {
+  type        = string
+  default     = "procurement"
+  description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
+}
