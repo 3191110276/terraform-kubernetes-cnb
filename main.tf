768eae0b09f69424f458ba8076b00164bd68d69b
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-13 14:41:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -377,3 +377,19 @@ module "accounting" {
 
   clusterload_configurations = var.accounting_clusterload_configurations
 }
+
+
+module "procurement_portal" {
+  depends_on = [module.accounting]
+  
+  source  = "./modules/production_portal"
+
+  namespace = var.procurement_namespace    
+    
+  procportal_name = var.procportal_name
+  replicas        = var.procportal_replicas
+  cpu_request     = var.procportal_cpu_request
+  memory_request  = var.procportal_memory_request
+  cpu_limit       = var.procportal_cpu_limit
+  memory_limit    = var.procportal_memory_limit
+}
