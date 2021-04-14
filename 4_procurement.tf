5d941da568ac2f0413c6a9635b798d9943415fc8
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-14 23:30:41 +0200

    Update 4_procurement.tf

--- 4_procurement.tf
+++ 4_procurement.tf
@@ -1,8 +1,16 @@
 ############################################################
 # PROCUREMENT APPLICATION
 ############################################################
+module "procurement_base" {
+  depends_on = [module.accounting, kubernetes_namespace.procurement]
+  
+  source  = "./modules/procurement_base"
+
+  namespace = var.procurement_namespace
+}
+
 module "procurement_helm" {
-  depends_on = [kubernetes_namespace.procurement]
+  depends_on = [module.procurement_base]
   
   source  = "./modules/procurement"
 }
