dba3e72e0e8b0c2d735296b7c575749bf1c9385e
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-14 23:57:06 +0200

    Update 4_procurement.tf

--- 4_procurement.tf
+++ 4_procurement.tf
@@ -8,9 +8,26 @@ module "procurement_base" {
 
   namespace = var.procurement_namespace
 }
+  
+  
+module "procurement_prediction" {
+  depends_on = [module.procurement_base]
+  
+  source  = "./modules/procurement_prediction"
+
+  namespace = var.procurement_namespace 
+    
+  procprediction_name = var.procprediction_name
+  replicas            = var.procprediction_replicas
+  cpu_request         = var.procprediction_cpu_request
+  memory_request      = var.procprediction_memory_request
+  cpu_limit           = var.procprediction_cpu_limit
+  memory_limit        = var.procprediction_memory_limit
+}
+
 
 module "procurement_helm" {
-  depends_on = [module.procurement_base]
+  depends_on = [module.procurement_prediction]
   
   source  = "./modules/procurement"
 }
