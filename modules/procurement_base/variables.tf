39a437e1bbfe187a283c7db86e6c640d1bf62931
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-15 11:07:40 +0200

    Update variables.tf

--- modules/procurement_base/variables.tf
+++ modules/procurement_base/variables.tf
@@ -12,3 +12,39 @@ variable "pod_quota" {
   default     = 100
   description = "Quota for Pods"
 }
+
+variable "cpu_request_quota" {
+  type        = string
+  default     = "6"
+  description = "Quota for requests.cpu"
+}
+
+variable "cpu_limit_quota" {
+  type        = string
+  default     = "6"
+  description = "Quota for limits.cpu"
+}
+
+variable "memory_request_quota" {
+  type        = string
+  default     = "30G"
+  description = "Quota for requests.memory"
+}
+
+variable "memory_limit_quota" {
+  type        = string
+  default     = "30G"
+  description = "Quota for limits.memory"
+}
+
+variable "pvc_quota" {
+  type        = number
+  default     = 5
+  description = "Quota for PVCs"
+}
+
+variable "storage_quota" {
+  type        = number
+  default     = "100G"
+  description = "Quota for storage"
+}
