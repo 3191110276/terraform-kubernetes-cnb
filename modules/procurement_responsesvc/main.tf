e7f48382f3b82937c532031bdeabc18706a4a2ba
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-14 21:45:26 +0200

    Update main.tf

--- modules/procurement_responsesvc/main.tf
+++ modules/procurement_responsesvc/main.tf
@@ -97,7 +97,7 @@ resource "kubernetes_deployment" "action-response-services" {
   depends_on = [kubernetes_config_map.responsesvc]
   
   timeouts {
-    create = "900s"
+    create = "1800s"
   }
   
   metadata {
