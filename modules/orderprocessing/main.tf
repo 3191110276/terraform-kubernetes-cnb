92589cc7f551f844b59a4ebe137c39f10b825a9c
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-14 21:43:11 +0200

    Update main.tf

--- modules/orderprocessing/main.tf
+++ modules/orderprocessing/main.tf
@@ -44,7 +44,7 @@ resource "kubernetes_service" "orderprocessing" {
 
 resource "kubernetes_deployment" "orderprocessing" {
   timeouts {
-    create = "900s"
+    create = "1800s"
   }
   
   metadata {
