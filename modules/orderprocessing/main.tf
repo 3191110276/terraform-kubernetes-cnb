a4be789b0002fed4826ec5a1e6a654a14788d3a9
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-15 11:01:54 +0200

    Update main.tf

--- modules/orderprocessing/main.tf
+++ modules/orderprocessing/main.tf
@@ -59,6 +59,8 @@ resource "kubernetes_deployment" "orderprocessing" {
 
   spec {
     replicas = var.replicas
+    
+    progress_deadline_seconds = "3600s"
 
     selector {
       match_labels = {
