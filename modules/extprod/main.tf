d9095cfce6519861645ac2c9072420629f70c588
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-15 15:34:39 +0200

    Update main.tf

--- modules/extprod/main.tf
+++ modules/extprod/main.tf
@@ -121,7 +121,7 @@ resource "kubernetes_deployment" "extprod" {
   spec {
     replicas = var.replicas
     
-    progress_deadline_seconds = "3600s"
+    progress_deadline_seconds = 3600
 
     selector {
       match_labels = {
