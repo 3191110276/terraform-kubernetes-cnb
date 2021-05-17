858769c27900cf78704fa884541dbaa97a8fee44
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-17 17:55:34 +0200

    Update variables.tf

--- modules/procurement_responsesvc/variables.tf
+++ modules/procurement_responsesvc/variables.tf
@@ -6,3 +6,29 @@ variable "namespace" {
   default     = "procurement"
   description = "Namespace used for deploying the object. This namespace has to exist and is not provisioned by this submodule."
 }
+
+variable "procurement_app_name" {
+  type        = string
+  default     = "cnb-procurement"
+  description = "The name of the procurement application."
+}
+
+variable "appd_account_name" {
+  type        = string
+  description = "Account name of the AppDynamics account."
+}
+
+variable "appd_controller_hostname" {
+  type        = string
+  description = "Hostname of the AppDynamics controller."
+}
+
+variable "appd_controller_port" {
+  type        = string
+  description = "Port of the AppDynamics controller."
+}
+
+variable "appd_controller_key" {
+  type        = string
+  description = "Controller key of the AppDynamics controller."
+}
