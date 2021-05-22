115b23021a5c8916dddf22952771ea438fc39948
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-22 15:50:11 +0200

    Update 5_chaosmesh.tf

--- 5_chaosmesh.tf
+++ 5_chaosmesh.tf
@@ -12,3 +12,23 @@ module "chaosmesh" {
 ############################################################
 # CREATE CHAOS MESH PROBLEMS
 ############################################################
+module "chaosmesh_failures" {
+  depends_on = [module.chaosmesh]
+  
+  source  = "./modules/chaosmesh_failures"
+    
+  order_app_name = var.order_app_name
+    
+  order_namespace       = var.order_namespace
+  ext_namespace         = var.extpayment_namespace
+  production_namespace  = var.extprod_namespace
+  procurement_namespace = var.procurement_namespace
+    
+  extprod_name        = var.extprod_name
+  extpayment_name     = var.extpayment_name
+  fulfilment_name     = var.fulfilment_name
+  adminfile_name      = var.adminfile_name
+  procprediction_name = var.procprediction_name
+  procexternal_name   = var.procexternal_name
+  procedgeagg_name    = var.procedgeagg_name
+}
