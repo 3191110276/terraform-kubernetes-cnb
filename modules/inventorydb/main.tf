163dbe55ff8d85233ed2e17f89e9c36d8f753d0f
Michael Maurer <3191110276@users.noreply.github.com>
2021-06-19 15:13:32 +0200

    Update main.tf

--- modules/inventorydb/main.tf
+++ modules/inventorydb/main.tf
@@ -7,6 +7,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.0.2"
     }
+    kubernetes-alpha = {
+      source = "hashicorp/kubernetes-alpha"
+      version = "0.5.0"
+    }
   }
 }
 
